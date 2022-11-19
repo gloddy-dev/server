d8300b955f3178ed420fea4061892643e43e14c7
twoosky <lhjae3@naver.com>
2022-11-19 21:18:58 +0900

    feat: User에 imageUrl 컬럼 추가

--- src/main/java/com/gloddy/server/auth/entity/User.java
+++ src/main/java/com/gloddy/server/auth/entity/User.java
@@ -34,6 +34,9 @@ public class User extends BaseTimeEntity {
     @Column(name = "password", columnDefinition = "longtext")
     private String password;
 
+    @Column(name = "image_url", columnDefinition = "longtext")
+    private String imageUrl;
+
     private String name;
 
     @Column(name = "school")
